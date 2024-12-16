666a5584143535b9be2179fec9fa4ac32e90e9c3
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2024-12-16 18:36:06 +0100

    x

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -42,6 +42,9 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "virtio_gpu" ];
+  boot.kernelParams = [ "console=tty" ];
+
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
