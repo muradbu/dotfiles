fdbfa3706b6b918cebc01b95375983a252cb1d1c
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-04-24 21:01:37 +0200

    neoscroll

--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -6,6 +6,28 @@
     colorschemes.ayu.enable = true;
     plugins = {
       nix.enable = true;
+      neoscroll = {
+        enable = true;
+        autoLoad = true;
+        settings = {
+          cursor_scrolls_alone = true;
+          easing_function = "quadratic";
+          hide_cursor = true;
+          mappings = [
+            "<C-u>"
+            "<C-d>"
+            "<C-b>"
+            "<C-f>"
+            "<C-y>"
+            "<C-e>"
+            "zt"
+            "zz"
+            "zb"
+          ];
+          respect_scrolloff = false;
+          stop_eof = true;
+        };
+      };
       lualine.enable = true;
       numbertoggle.enable = true;
       treesitter = {
