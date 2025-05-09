7a1202e45f579392dc3adac21c2cf1506cdcda8f
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-05-09 23:45:12 +0200

    x

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -147,8 +147,8 @@
   '';
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
-  networking.firewall.allowedUDPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 8096 ];
+  networking.firewall.allowedUDPPorts = [ 80 443 8096 ];
   # Or disable the firewall altogether.
   #networking.firewall.enable = false;
 
