0b837db5a5d954c048ba77b49a55980bf6be5e25
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-05-10 20:51:02 +0200

    x

--- hosts/home/disks.nix
+++ hosts/home/disks.nix
@@ -1,17 +1,12 @@
 { ... }:
 {
-  # Decrypting and mounting luks encrypted disks in the front bays
-
-  environment.etc.crypttab = {
-    mode = "0600";
-    text = ''
-      # data01 /dev/mapper/data01 /root/data01.key
-      data01 UUID=edc55baf-dc30-4730-acb5-346d998ef511 /root/data01.key
-    '';
+  fileSystems."/mnt/data01" = {
+    device = "/dev/disk/by-label/data01";
+    fsType = "xfs";
   };
 
-  fileSystems."/mnt/data01" = {
-    device = "/dev/mapper/data01";
+  fileSystems."/mnt/data02" = {
+    device = "/dev/disk/by-label/data02";
     fsType = "xfs";
   };
 }
