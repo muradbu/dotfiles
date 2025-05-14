addf11c1bae612d961106b29b8d7586f07cd9dd7
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-05-14 22:48:20 +0200

    x

--- hosts/homeserver/home.nix
+++ hosts/homeserver/home.nix
@@ -8,15 +8,17 @@
     htop
   ];
 
-  programs.bash.enable = true;
-  programs.bash.profileExtra = ''
-    if command -v fzf-share >/dev/null; then
-      source "$(fzf-share)/key-bindings.bash"
-      source "$(fzf-share)/completion.bash"
-    fi
+  programs.bash = {
+    enable = true;
+    profileExtra = ''
+      if command -v fzf-share >/dev/null; then
+        source "$(fzf-share)/key-bindings.bash"
+        source "$(fzf-share)/completion.bash"
+      fi
 
-    eval "$(/run/current-system/sw/bin/z --init bash fzf)"
-  '';
+      eval "$(/run/current-system/sw/bin/z --init bash fzf)"
+    '';
+  };
 
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
