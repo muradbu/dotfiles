bd871c0b71ddeafeaf482ffeb6e183e5d878d907
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-05-14 21:57:33 +0200

    x

--- hosts/homeserver/configuration.nix
+++ hosts/homeserver/configuration.nix
@@ -15,7 +15,16 @@
   home-manager.users.murad = { pkgs, ... }: {
     #home.packages = with pkgs; [ ];
 
-    home.stateVersion = "24.05";
+    programs.bash.profileExtra = ''
+      if command -v fzf-share >/dev/null; then
+        source "$(fzf-share)/key-bindings.bash"
+        source "$(fzf-share)/completion.bash"
+      fi
+
+      eval "$(/run/current-system/sw/bin/z --init bash fzf)"
+    '';
+
+    home.stateVersion = "24.11";
   };
 
   environment.systemPackages = with pkgs; [
