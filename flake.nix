11daad04f3cc54be922f9bfdf7e1cf1d9c601ef2
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2026-02-14 01:21:57 +0100

    x

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Murad's Infrastructure Platform";
+  description = "Murad's dotfiles";
 
   inputs = {
     colmena.url = "github:zhaofengli/colmena";
@@ -8,14 +8,12 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-generators.url = "github:nix-community/nixos-generators";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
     # https://flake.parts/module-arguments.html
     flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
       imports = [
@@ -24,18 +22,12 @@
         # inputs.foo.flakeModules.default
       ];
       flake = {
-        nixosModules = {
-          core = ./modules/core;
-          hardware = ./modules/hardware;
+
+        nixosConfigurations."lxc-template" = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./hosts/lxc-shared.nix
+          ];
         };
-        nixosConfigurations = 
-          let
-            mkHost = import ./lib/mkHost.nix { inherit inputs; };
-            discoverHosts = import ./lib/host-discovery.nix { inherit inputs mkHost; };
-          in
-          discoverHosts { baseDir = ./hosts/virtual; };
-        
-        colmena = import ./lib/mkColmena.nix { inherit inputs; } { baseDir = ./hosts/virtual; };
       };
       systems = [
         # systems for which you want to build the `perSystem` attributes
@@ -44,19 +36,8 @@
         "aarch64-darwin"
         # ...
       ];
-      perSystem = { config, pkgs, system, self', ... }: {
+      perSystem = { config, pkgs, ... }: {
         formatter = pkgs.nixpkgs-fmt;
-        
-        packages = if system == "x86_64-linux" then {
-          lxc-image = inputs.nixos-generators.nixosGenerate {
-            inherit pkgs;
-            format = "proxmox-lxc";
-            modules = [
-              ./hosts/bootstrap/lxc/default.nix
-            ];
-          };
-        } else {};
-
         # Recommended: move all package definitions here.
         # e.g. (assuming you have a nixpkgs input)
         # packages.foo = pkgs.callPackage ./foo/package.nix { };
