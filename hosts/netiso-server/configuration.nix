eaf13e25efe56ac604ece22fd174f85c065cbd7c
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2026-04-11 20:26:51 +0200

    vsftp

--- hosts/netiso-server/configuration.nix
+++ hosts/netiso-server/configuration.nix
@@ -9,4 +9,18 @@
     lftp
     maxcso
   ];
+
+  services.vsftpd = {
+    enable = true;
+    localUsers = true;
+    writeEnable = true;
+    chrootlocalUser = true;
+    extraConfig = ''
+      pasv_enable=YES
+    '';
+
+    anonymousUser = true;
+    anonymousUserNoPassword = true;
+    anonymousUserHome = "/home/murad/games";
+  };
 }
