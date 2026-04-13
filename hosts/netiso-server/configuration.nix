1b8eb98d2b89039653336413d9a6fbbaeb324b72
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2026-04-14 00:20:08 +0200

    netiso additions

--- hosts/netiso-server/configuration.nix
+++ hosts/netiso-server/configuration.nix
@@ -7,6 +7,7 @@
 
   environment.systemPackages = with pkgs; [
     lftp
+    _7zz
     maxcso
   ];
 
--- shells/default.nix
+++ shells/default.nix
@@ -1,10 +1,38 @@
 {
-  perSystem = { pkgs, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "infra-shell";
-      packages = with pkgs; [
-        colmena
-      ];
+  perSystem =
+    { pkgs, ... }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "infra-shell";
+        packages = with pkgs; [
+          colmena
+        ];
+      };
+
+      # devshell to compile https://github.com/muradbu/XGDTool
+      devShells.xgdtool = pkgs.mkShell {
+
+        # Tools needed to build the project
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          cmake
+          gnumake
+        ];
+
+        # Libraries needed for linking
+        buildInputs = with pkgs; [
+          lz4
+          zstd
+          openssl
+          curl
+          wxwidgets_3_2
+        ];
+
+        # Optional: A welcome message when you enter the shell
+        shellHook = ''
+          echo "C/C++ development shell active."
+          echo "Compiler toolchain, CMake, and wxWidgets are ready."
+        '';
+      };
     };
-  };
 }
