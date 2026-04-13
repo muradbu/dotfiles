5aa3ebe1613b2a1af3dd918e7a414b05ab7d9009
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2026-04-14 00:46:13 +0200

    local bin path

--- hosts/netiso-server/configuration.nix
+++ hosts/netiso-server/configuration.nix
@@ -22,7 +22,7 @@
     after = [ "network.target" ];
 
     serviceConfig = {
-      ExecStart = "/home/murad/.local/bin/netiso-srv -r";
+      ExecStart = "/home/murad/.local/bin/netiso-srv -r .";
 
       WorkingDirectory = "/home/murad/games/xbox360/extracted";
       User = "murad";
