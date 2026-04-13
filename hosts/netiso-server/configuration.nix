dad1998907b06113fc414236dfc9b3173615763e
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2026-04-14 00:35:42 +0200

    systemd service file

--- hosts/netiso-server/configuration.nix
+++ hosts/netiso-server/configuration.nix
@@ -11,6 +11,28 @@
     maxcso
   ];
 
+  systemd.services.netiso = {
+    enable = true;
+    description = "NetISO Server for Xbox 360";
+
+    # This ensures the service starts automatically on boot
+    wantedBy = [ "multi-user.target" ];
+
+    # Wait for the network to be up before starting
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      ExecStart = "/home/murad/netiso-srv -r";
+
+      WorkingDirectory = "/home/murad/games/xbox360/extracted";
+      User = "murad";
+
+      # Ninja standard: Auto-restart if the server crashes
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
+
   services.vsftpd = {
     enable = true;
     localUsers = true;
